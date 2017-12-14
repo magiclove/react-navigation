d6a37cf0ee00add6cec7d88d32f8c65480e5992a
刘云霞 <liuyunxia@liuyunxiadeMac-mini.local>
2017-12-14 16:50:51 +0800

    添加多层跳转

--- src/views/ScenesReducer.js
+++ src/views/ScenesReducer.js
@@ -151,7 +151,17 @@ export default function ScenesReducer(
     }
   };
 
-  staleScenes.forEach(mergeScene);
+  let k = null;
+  let v = null;
+  staleScenes.forEach(scene => {
+    let {key} = scene;
+    k = key;
+    v = scene
+  });
+  const newStaleScenes = k && v ? new Map([[k, v]]) : new Map();
+  newStaleScenes.forEach(mergeScene)
+
+  // staleScenes.forEach(mergeScene);
   freshScenes.forEach(mergeScene);
 
   nextScenes.sort(compareScenes);
